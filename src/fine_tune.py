ba28e05ed5711b49446e7a857eb87e226559569e
Jingjing Zhai <zhaijingjing603@gmail.com>
2024-07-21 20:39:18 -0700

    Enable testing only

--- src/fine_tune.py
+++ src/fine_tune.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import xgboost as xgb
 from sklearn import metrics
 import matplotlib.pyplot as plt
+import logging
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,6 +20,7 @@ def parse_args():
     parser.add_argument("-device", type=str, default="cuda:0", help="The device to run the model")
     parser.add_argument("-batchSize", type=int, default=128, help="The batch size for the model")
     parser.add_argument("-tokenIdx", type=int, default=255, help="The index of the nucleotide")
+    parser.add_argument("-test_only", action='store_true', help="Flag to perform only testing")
     return parser.parse_args()
 
 class SequenceDataset(Dataset):
@@ -44,24 +46,28 @@ class SequenceDataset(Dataset):
         }
 
 def load_data(filepath):
+    logging.info(f"Loading data from {filepath}")
     data = pd.read_csv(filepath, delimiter='\t')
     return data['sequences'].tolist(), data['label'].tolist()
 
 def create_dataloader(sequences, tokenizer, batch_size):
+    logging.info(f"Creating DataLoader with batch size {batch_size}")
     dataset = SequenceDataset(sequences, tokenizer)
     return DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
 def load_model_and_tokenizer(model_dir, device):
+    logging.info(f"Loading pre-trained model and tokenizer from {model_dir}")
     model = AutoModelForMaskedLM.from_pretrained(model_dir, trust_remote_code=True)
     tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
     model.to(device)
     return model, tokenizer
 
 def extract_embeddings(model, dataloader, device, tokenIdx):
+    logging.info("Extracting embeddings")
     model.eval()
     embeddings = []
     with torch.no_grad():
-        for batch in tqdm(dataloader, desc="Extracting Embeddings"):
+        for batch in tqdm(dataloader):
             input_ids = batch['input_ids'].to(device)
             with torch.inference_mode():
                 outputs = model(input_ids=input_ids, output_hidden_states=True)
@@ -77,11 +83,13 @@ def extract_embeddings(model, dataloader, device, tokenIdx):
     return averaged_embeddings
 
 def train_xgboost_model(train_embeddings, train_labels, valid_embeddings, valid_labels):
+    logging.info("Training XGBoost model")
     model = xgb.XGBClassifier(n_estimators=1000, max_depth=6, learning_rate=0.1, n_jobs=64)
     model.fit(train_embeddings, train_labels, eval_set=[(valid_embeddings, valid_labels)])
     return model
 
 def evaluate_model(model, embeddings, labels):
+    logging.info("Evaluating model")
     predictions = model.predict_proba(embeddings)[:, 1]
     fpr, tpr, _ = metrics.roc_curve(labels, predictions)
     precision, recall, _ = metrics.precision_recall_curve(labels, predictions)
@@ -90,6 +98,7 @@ def evaluate_model(model, embeddings, labels):
     return fpr, tpr, precision, recall, roc_auc, prauc
 
 def plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, output_dir, prefix='valid'):
+    logging.info(f"Plotting metrics and saving to {output_dir}")
     fig, axs = plt.subplots(1, 2, figsize=(12, 6))
     axs[0].plot(fpr, tpr, label=f'AUC = {roc_auc:.2f}', linewidth=2)
     axs[0].set_title('ROC Curve')
@@ -107,32 +116,47 @@ def plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, output_dir, prefix
     plt.savefig(os.path.join(output_dir, prefix + '_metrics.png'))
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     args = parse_args()
     os.makedirs(args.output, exist_ok=True)
-    train_sequences, train_labels = load_data(args.train)
-    valid_sequences, valid_labels = load_data(args.valid)
 
     model, tokenizer = load_model_and_tokenizer(args.model, args.device)
 
-    train_loader = create_dataloader(train_sequences, tokenizer, args.batchSize)
-    valid_loader = create_dataloader(valid_sequences, tokenizer, args.batchSize)
+    if args.test_only:
+        if args.test:
+            test_sequences, test_labels = load_data(args.test)
+            test_loader = create_dataloader(test_sequences, tokenizer, args.batchSize)
+            test_embeddings = extract_embeddings(model, test_loader, args.device, args.tokenIdx)
+            np.savez_compressed(os.path.join(args.output, 'test_embeddings.npz'), test=test_embeddings)
+            
+            xgb_model = xgb.XGBClassifier()
+            xgb_model.load_model(os.path.join(args.output, 'model.json'))
+            
+            fpr, tpr, precision, recall, roc_auc, prauc = evaluate_model(xgb_model, test_embeddings, test_labels)
+            plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, args.output, prefix='test')
+    else:
+        train_sequences, train_labels = load_data(args.train)
+        valid_sequences, valid_labels = load_data(args.valid)
 
-    train_embeddings = extract_embeddings(model, train_loader, args.device, args.tokenIdx)
-    valid_embeddings = extract_embeddings(model, valid_loader, args.device, args.tokenIdx)
-    np.savez_compressed(os.path.join(args.output, 'train_valid_embeddings.npz'), train=train_embeddings, valid=valid_embeddings)
+        train_loader = create_dataloader(train_sequences, tokenizer, args.batchSize)
+        valid_loader = create_dataloader(valid_sequences, tokenizer, args.batchSize)
 
-    xgb_model = train_xgboost_model(train_embeddings, train_labels, valid_embeddings, valid_labels)
-    xgb_model.save_model(os.path.join(args.output, 'model.json'))
-    fpr, tpr, precision, recall, roc_auc, prauc = evaluate_model(xgb_model, valid_embeddings, valid_labels)
-    plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, args.output, prefix='valid')
+        train_embeddings = extract_embeddings(model, train_loader, args.device, args.tokenIdx)
+        valid_embeddings = extract_embeddings(model, valid_loader, args.device, args.tokenIdx)
+        np.savez_compressed(os.path.join(args.output, 'train_valid_embeddings.npz'), train=train_embeddings, valid=valid_embeddings)
 
-    if args.test:
-        test_sequences, test_labels = load_data(args.test)
-        test_loader = create_dataloader(test_sequences, tokenizer, args.batchSize)
-        test_embeddings = extract_embeddings(model, test_loader, args.device, args.tokenIdx)
-        np.savez_compressed(os.path.join(args.output, 'test_embeddings.npz'), test=test_embeddings)
-        fpr, tpr, precision, recall, roc_auc, prauc = evaluate_model(xgb_model, test_embeddings, test_labels)
-        plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, args.output, prefix='test')
+        xgb_model = train_xgboost_model(train_embeddings, train_labels, valid_embeddings, valid_labels)
+        xgb_model.save_model(os.path.join(args.output, 'model.json'))
+        fpr, tpr, precision, recall, roc_auc, prauc = evaluate_model(xgb_model, valid_embeddings, valid_labels)
+        plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, args.output, prefix='valid')
+
+        if args.test:
+            test_sequences, test_labels = load_data(args.test)
+            test_loader = create_dataloader(test_sequences, tokenizer, args.batchSize)
+            test_embeddings = extract_embeddings(model, test_loader, args.device, args.tokenIdx)
+            np.savez_compressed(os.path.join(args.output, 'test_embeddings.npz'), test=test_embeddings)
+            fpr, tpr, precision, recall, roc_auc, prauc = evaluate_model(xgb_model, test_embeddings, test_labels)
+            plot_metrics(fpr, tpr, precision, recall, roc_auc, prauc, args.output, prefix='test')
 
 if __name__ == "__main__":
     main()
